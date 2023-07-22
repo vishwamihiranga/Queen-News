da6ed209306fe25250f5c0ae2ce4f0acad6917be
DarkWinzo <darkwinzo4440@gmail.com>
2023-07-22 21:17:50 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+
+module.exports = {
+  PREFIX: ".", //set Prefix
+  OWNER: "94775200935",//Enter Owner Number
+  USER_NAME: "user1",//Enter Your UserName
+  PASSWORD: "password1",//Enter Your Password
+  GROUP_JID: ["120363161816871173@g.us"],//News Group Jid
+  NASA_GROUP_JID: ["120363161816871173@g.us"],//Nasa Group Jid
+  TECH_GROUP_JID: ["120363161816871173@g.us"]//Tech Group Jid
+};
