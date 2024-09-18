4df61cc933bc313b33e5a747dbf77a338f7d9bd1
DarkWinzo <darkwinzo4440@gmail.com>
2024-09-18 18:23:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 module.exports = {
   PREFIX: ".", //set Prefix
   OWNER: "94775200935",//Enter Owner Number
-  USER_NAME: "****",//Enter Your UserName (contact Darkwinzo for get username and password)
+  USER_NAME: "****",//Enter Your UserName (contact Darkwinzo for get username and password +94775200935)
   PASSWORD: "********",//Enter Your Password
   GROUP_JID: ["*******"],//News Group Jid
 };
