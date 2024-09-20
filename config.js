a4547451e1e5b13bbacaaee327071f35417ed374
Vishwa Mihiranga <145928580+vishwamihiranga@users.noreply.github.com>
2024-09-20 06:04:48 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 
 module.exports = {
-  PREFIX: ".", //set Prefix
-  OWNER: "94775200935",//Enter Owner Number
-  USER_NAME: "****",//Enter Your UserName (contact Darkwinzo for get username and password +94775200935)
-  PASSWORD: "********",//Enter Your Password
-  GROUP_JID: ["*******"],//News Group Jid
+  PREFIX: ",", //set Prefix
+  OWNER: "94702481115",//Enter Owner Number
+  USER_NAME: "vishwa",//Enter Your UserName (contact Darkwinzo for get username and password +94775200935)
+  PASSWORD: "$newscore#vishwax",//Enter Your Password
+  GROUP_JID: ["120363335688758632@g.us"],//News Group Jid
 };
 
